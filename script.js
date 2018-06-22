13f57d129c0ab2df634f7232cdfedf51deeddce0
kalebrah007 <bayarea.collias@gmail.com>
2018-06-22 15:31:17 -0400

    added loop

---
+++ script.js
@@ -0,0 +1,9 @@
+var friendName = ['aaron', 'brad', 'carl', 'denise', 'emily'];
+var locationName = ['bathroom', 'closet', 'copy room', 'training room', 'accounting', 'payroll', 'hr', 'qual', 'breakroom', 'conference room'];
+var weaponName = ['axe', 'hammer', 'screwdriver', 'saw', 'bamboo stik', 'nail gun', 'drill', 'rope', 'helmet', 'tire iron',
+'lug wrench', 'knife', 'revolver', 'shotgun', 'cable', 'candlestick', 'lead pipe', 'harpoon', 'bomb', '2x4'];
+var times = 1;
+while (times<101){
+  console.log('accusation', times);
+  times++;
+}
