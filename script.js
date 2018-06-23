6ac9a05c5b5abd6718be0a8818a1075d6bac55e5
kalebrah007 <bayarea.collias@gmail.com>
2018-06-23 11:47:56 -0400

    update script.js

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-
-
-var friendName = ['aaron', 'brad', 'carl', 'denise', 'emily'];
-var locationName = ['bathroom', 'closet', 'copy room', 'training room', 'accounting', 'payroll', 'hr', 'qual', 'breakroom', 'conference room'];
-var weaponName = ['axe', 'hammer', 'screwdriver', 'saw', 'bamboo stik', 'nail gun', 'drill', 'rope', 'helmet', 'tire iron',
+var _friendName = ['aaron', 'brad', 'carl', 'denise', 'emily'];
+var _locationName = ['bathroom', 'closet', 'copy room', 'training room', 'accounting', 'payroll', 'hr', 'qual', 'breakroom', 'conference room'];
+var _weaponName = ['axe', 'hammer', 'screwdriver', 'saw', 'bamboo stik', 'nail gun', 'drill', 'rope', 'helmet', 'tire iron',
 'lug wrench', 'knife', 'revolver', 'shotgun', 'cable', 'candlestick', 'lead pipe', 'harpoon', 'bomb', '2x4'];
+var accusation = {_friendName,_locationName,_weaponName}
+for(i=1; i< accusation.length; i++)
+var btn = document.getElementById('big-button');
+btn.addEventListener('click', function accusation(){
+  console.log(accusation);
+});
 
-for (var i=1)
+
