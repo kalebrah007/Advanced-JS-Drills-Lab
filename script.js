dead740eab51ac9dabfe71d385e1f8df01fc4a8f
kalebrah007 <bayarea.collias@gmail.com>
2018-06-23 11:08:01 -0400

    update html

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
+
+
 var friendName = ['aaron', 'brad', 'carl', 'denise', 'emily'];
 var locationName = ['bathroom', 'closet', 'copy room', 'training room', 'accounting', 'payroll', 'hr', 'qual', 'breakroom', 'conference room'];
 var weaponName = ['axe', 'hammer', 'screwdriver', 'saw', 'bamboo stik', 'nail gun', 'drill', 'rope', 'helmet', 'tire iron',
 'lug wrench', 'knife', 'revolver', 'shotgun', 'cable', 'candlestick', 'lead pipe', 'harpoon', 'bomb', '2x4'];
-var times = 1;
-while (times<101){
-  console.log('accusation', times);
-  times++;
-}
+
+for (var i=1)
